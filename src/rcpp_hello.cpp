765c2ecc842c90c8d132ea992dffac6f653bf524
Gregory Jefferis <jefferis@gmail.com>
2015-06-24 12:11:32 +0100

    return materials as well as shapes

--- src/rcpp_hello.cpp
+++ src/rcpp_hello.cpp
@@ -32,10 +32,8 @@ List loadobj(std::vector< std::string > objfile) {
     stop(err);
   }
 
-  CharacterVector shapenames(shapes.size());
-  List z;
+  List sl, ml;
   for(unsigned int i=0; i<shapes.size(); i++) {
-    shapenames[i]=shapes[i].name;
     tinyobj::mesh_t m=shapes[i].mesh;
     /* typedef struct {
       std::vector<float> positions;
@@ -44,7 +42,42 @@ List loadobj(std::vector< std::string > objfile) {
       std::vector<unsigned int> indices;
       std::vector<int> material_ids; // per-mesh material ID
     } mesh_t; */
-    z[shapes[i].name]=List::create(m.positions, m.normals, m.texcoords, m.indices, m.material_ids);
+    sl[shapes[i].name]=List::create(m.positions, m.normals, m.texcoords,
+                                   m.indices, m.material_ids);
   }
-  return z;
+  for(unsigned int i=0; i<materials.size(); i++) {
+    tinyobj::material_t m=materials[i];
+    /* typedef struct {
+        std::string name;
+
+        float ambient[3];
+        float diffuse[3];
+        float specular[3];
+        float transmittance[3];
+        float emission[3];
+        float shininess;
+        float ior;      // index of refraction
+        float dissolve; // 1 == opaque; 0 == fully transparent
+        // illumination model (see http://www.fileformat.info/format/material/)
+        int illum;
+
+        std::string ambient_texname;
+        std::string diffuse_texname;
+        std::string specular_texname;
+        std::string normal_texname;
+        std::map<std::string, std::string> unknown_parameter;
+      } material_t; */
+    ml[m.name]=List::create(NumericVector::create(m.ambient[0], m.ambient[1], m.ambient[2]),
+                            NumericVector::create(m.diffuse[0], m.diffuse[1], m.diffuse[2]),
+                            NumericVector::create(m.specular[0], m.specular[1], m.specular[2]),
+                            NumericVector::create(m.transmittance[0], m.transmittance[1], m.transmittance[2]),
+                            NumericVector::create(m.emission[0], m.emission[1], m.emission[2]),
+                            m.shininess, m.ior, m.dissolve, m.illum,
+                            m.ambient_texname, m.diffuse_texname,
+                            m.specular_texname, m.normal_texname);
+  }
+  List r;
+  r["shapes"]=sl;
+  r["materials"]=ml;
+  return r;
 }
