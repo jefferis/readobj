8f52e84b5fea4a671bb2397b083f4191eb788892
Gregory Jefferis <jefferis@gmail.com>
2015-06-24 18:07:58 +0100

    update test baseline with change of loadobj sig

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -1,7 +1,7 @@
 context("loadobj")
 
 test_that("can load an obj file", {
-  expect_error(loadobj(c("foo","bar")), "one file")
+  expect_error(loadobj(c("foo","bar")), "single value")
   expect_error(loadobj("rhubarb"), "Error")
   expect_is(loadobj(system.file("obj/cube.obj", package = "tinyobjloader")),
             "list")
