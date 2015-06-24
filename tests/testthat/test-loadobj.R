b716f0c974aa91e6d658c139d9b02d1ef6991904
Gregory Jefferis <jefferis@gmail.com>
2015-06-24 18:09:07 +0100

    test read.obj
    the user visible function

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -1,8 +1,8 @@
 context("loadobj")
 
 test_that("can load an obj file", {
-  expect_error(loadobj(c("foo","bar")), "single value")
-  expect_error(loadobj("rhubarb"), "Error")
-  expect_is(loadobj(system.file("obj/cube.obj", package = "tinyobjloader")),
+  expect_error(read.obj(c("foo","bar")), "single value")
+  expect_error(read.obj("rhubarb"), "Error")
+  expect_is(read.obj(system.file("obj/cube.obj", package = "tinyobjloader")),
             "list")
 })
