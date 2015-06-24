dc3a18ad0d5b052635b97f3ede128c8c06fef2a4
Gregory Jefferis <jefferis@gmail.com>
2015-06-24 10:33:40 +0100

    add test data and try loading it

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -3,4 +3,6 @@ context("loadobj")
 test_that("can load an obj file", {
   expect_error(loadobj(c("foo","bar")), "one file")
   expect_error(loadobj("rhubarb"), "Error")
+  expect_is(loadobj(system.file("obj/cube.obj", package = "tinyobjloader")),
+            "list")
 })
