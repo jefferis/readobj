93d1030e7c2353d0820c4282818023e61577bc8d
Gregory Jefferis <jefferis@gmail.com>
2015-06-24 10:30:13 +0100

    skeleton test infrastructure

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(tinyobjloader)
+
+test_check("tinyobjloader")
---
+++ tests/testthat/test-loadobj.R
@@ -0,0 +1,5 @@
+context("loadobj")
+
+test_that("can load an obj file", {
+  expect_error(loadobj(c("foo","bar")), "one file")
+})
