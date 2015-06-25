af706e083b38bc540b14e3ad0327d73a064ecc8f
Gregory Jefferis <jefferis@gmail.com>
2015-06-25 17:22:58 +0100

    full test of structure for cube.obj

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -3,8 +3,12 @@ context("loadobj")
 test_that("can load an obj file", {
   expect_error(read.obj(c("foo","bar")), "single value")
   expect_error(read.obj("rhubarb"), "Error")
-  expect_is(read.obj(system.file("obj/cube.obj", package = "readobj")),
-            "list")
   expect_warning(read.obj(system.file("obj/cube_badmtl.obj", package = "readobj")),
                  'default material')
+
+  expect_is(cube<-read.obj(system.file("obj/cube.obj", package = "readobj")),
+            "list")
+  # saveRDS(cube, file='tests/testthat/testdata/cube.rds')
+  cube_baseline=readRDS("testdata/cube.rds")
+  expect_equal(cube, cube_baseline)
 })
