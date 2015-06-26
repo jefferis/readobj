e8e1e28ad3b22088bf6f59f4e05ebf2718659d24
Gregory Jefferis <jefferis@gmail.com>
2015-06-26 11:39:39 +0100

    add test for conversion to rgl format

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -12,3 +12,12 @@ test_that("can load an obj file", {
   cube_baseline=readRDS("testdata/cube.rds")
   expect_equal(cube, cube_baseline)
 })
+
+test_that("can convert to rgl format", {
+  if(require('rgl', quietly = TRUE)){
+    cube=readRDS("testdata/cube.rds")
+    cubesl_baseline=readRDS("testdata/cubesl.rds")
+    expect_is(cubesl <- tinyobj2shapelist3d(cube), 'shapelist3d')
+    expect_equal(cubesl, cubesl_baseline)
+  }
+})
