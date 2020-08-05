577ab2d8711f638bea3150ac23c8ab84700a39a4
Trevor L Davis <trevor.l.davis@gmail.com>
2020-08-05 11:07:40 -0700

    tests: New texture coordinates unit tests

--- tests/testthat/test-loadobj.R
+++ tests/testthat/test-loadobj.R
@@ -11,6 +11,12 @@ test_that("can load an obj file", {
   # saveRDS(cube, file='tests/testthat/testdata/cube.rds')
   cube_baseline=readRDS("testdata/cube.rds")
   expect_equal(cube, cube_baseline)
+
+  expect_is(tile<-read.obj(system.file("obj/tile.wavefront", package = "readobj")),
+            "list")
+  # saveRDS(tile, file='testdata/tile.rds')
+  tile_baseline=readRDS("testdata/tile.rds")
+  expect_equal(tile, tile_baseline)
 })
 
 test_that("can convert to rgl format", {
@@ -25,5 +31,35 @@ test_that("can convert to rgl format", {
         cubesl_baseline[[side]]$primitivetype <- NULL
     }
     expect_equal(cubesl, cubesl_baseline)
+
+    tile=readRDS("testdata/tile.rds")
+    expect_is(tilesl <- tinyobj2shapelist3d(tile), 'shapelist3d')
+    # saveRDS(tilesl, file='testdata/tilesl.rds')
+    tilesl_baseline=readRDS("testdata/tilesl.rds")
+    for (side in c("front", "back", "right", "left", "top", "bottom")) {
+      tilesl[[side]]$primitivetype <-
+        tilesl_baseline[[side]]$primitivetype <- NULL
+    }
+    expect_equal(tilesl, tilesl_baseline)
+  }
+})
+
+test_that("texture rendered correctly", {
+  skip_on_cran() # not sure how consistent rgl.snapshot() will be cross-platform
+  if(require('rgl', quietly = TRUE) && require('magick', quietly = TRUE)){
+      open3d()
+      view3d(phi=-45)
+      tilesl=readRDS("testdata/tilesl.rds")
+      texture=system.file("obj/tile.png", package="readobj")
+      shade3d(tilesl, material=list(texture=texture, color="white"))
+      # rgl.snapshot("testdata/snapshot.png")
+      snapshot <- tempfile(fileext=".png")
+      rgl.snapshot(snapshot)
+      rgl.close()
+
+      control=image_read("testdata/snapshot.png")
+      test=image_read(snapshot)
+      diff=image_compare(control, test, "AE")
+      expect_true(attr(diff, "distortion") < 0.01)
   }
 })
